185598e22b6c2fbbf24e7660199e29a109c97f6f
fsk <fsk@fskdev.co>
2023-11-28 22:25:48 +0300

    Initial commit

---
+++ collections/User.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+const writerSchema = new mongoose.Schema({
+    writerName: {
+        type: String,
+        required: true
+    },
+    writerSurname: {
+        type: String,
+        required: true
+    },
+    writerEmail: {
+        type: String,
+        required: true
+    },
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+}, {
+    timestamps: true
+});
+
+
+const Writer = mongoose.model('Writer', writerSchema);
+
+module.exports = Writer;
